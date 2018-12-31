28c7f7102fcce110152d7fe83064ecc0169f21ed
odivus <odivus@gmail.com>
2018-12-31 19:46:36 +0300

    Ok

--- js/show-days-for-new-year.js
+++ js/show-days-for-new-year.js
@@ -1,22 +1,6 @@
 (function() {
   'use strict';
   // Пример Сколько осталось дней до Нового года. Флэнаган стр. 792
-  //old
- /*  function getDaysForNewYear() {
-    let today = new Date(), // Запомнить сегодняшнюю дату
-      newYear = new Date(),
-      difference;
-
-    newYear.setMonth(11); // Установить месяц Декабрь
-    newYear.setDate(31); // Установить число
-
-    if (today.getTime() < newYear.getTime()) {
-      difference = newYear.getTime() - today.getTime(); // результат в миллисекундах
-      difference = Math.floor(difference / (1000 * 60 * 60 * 24));
-    }
-    return difference;
-  };
- */
   function getDaysForNewYear() {
     let today = new Date(), // Запомнить сегодняшнюю дату
       newYear = new Date(),
