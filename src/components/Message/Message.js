9eeb3b4e3a0b523a28fa237a5929c51e19189c53
odivus <odivus@gmail.com>
2022-05-08 19:56:24 +0300

    Message component added

---
+++ src/components/Message/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Message.css';
+
+function Message() {
+  return (
+    <div className='message'>
+      В&nbsp;связи с&nbsp;санкционной блокировкой сервиса Firebase, приложение не&nbsp;работает.
+    </div>
+  );
+}
+
+export default Message;
