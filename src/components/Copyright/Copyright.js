54730134ccd239214ec0b05c6f0ce57d06656b54
odivus <odivus@gmail.com>
2022-05-08 19:33:11 +0300

    Copyright year now added

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Copyright.css';
 
 function Copyright() {
+  const yearNow = new Date().getFullYear();
   return (
     <div className='copyright'>
-      &copy;&nbsp;2021&nbsp;Олег Петько
+      &copy;&nbsp;2021&ndash;{yearNow} Олег&nbsp;Петько
     </div>
   );
 }
